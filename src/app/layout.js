282c0e7c5748b266c43e56764223aa8087d90a28
unknown <rakibulhasanridoy1@gmail.com>
2026-05-01 21:23:03 +0600

    Body

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Script from 'next/script'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/Navbar";
+import Body from "@/Components/Body";
 
 
 const geistSans = Geist({
@@ -29,7 +30,7 @@ export default function RootLayout({ children }) {
         <Navbar/>
         
         {children}
-        
+        <Body/>
         
         <Script src="https://cdn.tailwindcss.com" strategy="beforeInteractive" />
 <Script id="tailwind-config" strategy="afterInteractive">
