836a43448d1d9380226641bdb4a6413de7feb6e8
unknown <rakibulhasanridoy1@gmail.com>
2026-05-07 01:28:06 +0600

    fix: add useRef import

--- src/Components/UpdateProfile.jsx
+++ src/Components/UpdateProfile.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import { authClient } from '@/lib/auth-client'
